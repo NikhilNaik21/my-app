3b99f66cb5cd03535f308a691771805df1e7208c
my name <my@example.com>
2023-05-21 11:57:47 +0530

    React First App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code></code> Nikhil Naik 
         </p>
         <a
           className="App-link"
